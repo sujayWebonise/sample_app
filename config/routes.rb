e82c486357785d9b7b1d13ccd079ab6b0f445fb6
Sujay Gavhane <sujay.gavhane@weboniselab.com>
2015-01-23 11:45:10 +0530

    problem with follow button solved

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
